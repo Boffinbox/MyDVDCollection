f0ebf8567ef15c0604e15dae5c2caf92c9e1691e
Boffinbox <boffinbox@hotmail.co.uk>
2023-09-26 01:41:48 +0100

    barely functional client server connection after 6 hours
    probably not type safe but i dont care at this point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
+import axios from "axios"
 import "./App.css"
+
+import { useState, useEffect } from "react"
+
+interface IData
+{
+    dvds: string[]
+}
 
 function App()
 {
+    const [data, setData] = useState<IData>({ dvds: ["test"] })
+
+    useEffect(() =>
+    {
+        axios.get("/api")
+            .then((response) =>
+            {
+                console.log(response.data);
+                setData(response.data);
+            })
+    }, [])
+
     return (
         <div>
-            test
+            {data.dvds.map((d, i) => <div key={i}>{d}</div>)}
         </div>
     )
 }
