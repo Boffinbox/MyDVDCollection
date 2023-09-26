30618176c4a1c623ec98ab58db70f6a947f33bbc
Boffinbox <boffinbox@hotmail.co.uk>
2023-09-26 18:00:56 +0100

    add try catch async helper to pass errors immediately to fallback

---
+++ server/helpers/TryCatchAsync.ts
@@ -0,0 +1,13 @@
+// accepts a function, and then executes that function
+// if any errors are found, they are caught and passed
+// to an error middleware in express
+
+// intended for use with async express middleware
+
+export function TryCatchAsync(fn)
+{
+    return function (req, res, next)
+    {
+        fn(req, res, next).catch((e) => next(e));
+    }
+}
