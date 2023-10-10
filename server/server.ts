45aece2d4a1195bd84f6027cafec8896dc05be33
Boffinbox <boffinbox@hotmail.co.uk>
2023-10-10 19:08:24 +0100

    update typing for patch route now safer

--- server/server.ts
+++ server/server.ts
@@ -141,7 +141,7 @@ app.post("/api/v1/disccollections/:collectionId/dvds/:barcode", TryCatchAsync(as
 // update a dvd in a collection by discId
 app.patch("/api/v1/disccollections/:collectionId/dvds/:discId", TryCatchAsync(async (req, res, next) =>
 {
-    const { collectionId, discId } = req.params
+    const { collectionId, discId }: { collectionId: string, discId: string } = req.params
     const { rating = 0, watched = false }: { rating: number, watched: boolean } = req.body;
     console.log("Someone tried to use API to update a dvd in a disc collection");
     console.log(`using the collId ${collectionId} and discId ${discId}`)
@@ -162,13 +162,13 @@ app.patch("/api/v1/disccollections/:collectionId/dvds/:discId", TryCatchAsync(as
     if (!discInCollection)
     {
         console.log("we couldn't find the disc, rip");
-        return res.status(400).json({ message: "couldn't find dvd with this barcode" });
+        return res.status(400).json({ message: "couldn't find dvd with this discId" });
     }
     const discToModify = await DVDModel.findById(discInCollection._id);
     if (!discToModify)
     {
         console.log("Couldn't find disc despite having disc??, aborting...");
-        res.status(400).json({ message: "couldn't find dvd with this barcode" });
+        res.status(400).json({ message: "stop trying to modify dvds that aren't yours!" });
     }
     else
     {
