a1f08d7b54d8cff0c8189734221d9d9bac3c75d8
Boffinbox <boffinbox@hotmail.co.uk>
2023-10-11 17:23:22 +0100

    refactor disccollection routes out to routing file

---
+++ server/routes/disccollections.ts
@@ -0,0 +1,76 @@
+const express = require("express")
+const router = express.Router();
+
+import { TryCatchAsync } from "../helpers/TryCatchAsync"
+import
+{
+    ReferenceDVDModel,
+    DVDModel,
+    DiscCollectionModel
+} from "../models/models"
+
+// disc collection restful routing
+// index a list of all disc collections (in future: only DCs that user is authorized to see)
+router.get("/", TryCatchAsync(async (req, res, next) =>
+{
+    const allCollections = await DiscCollectionModel
+        .find({})
+        .populate({
+            path: "discs",
+            populate: {
+                path: "referenceDVD"
+            }
+        })
+        .exec();
+    const returnString = JSON.stringify(allCollections);
+    res.status(200).send(returnString);
+}))
+
+// show individual disc collection
+router.get("/:collectionId", TryCatchAsync(async (req, res, next) =>
+{
+    console.log(req.params.collectionId);
+    const collectionOfConcern = await DiscCollectionModel
+        .findOne({ _id: req.params.collectionId })
+        .populate({
+            path: "discs",
+            populate: {
+                path: "referenceDVD"
+            }
+        })
+        .exec();
+    const returnString = JSON.stringify(collectionOfConcern);
+    res.status(200).send(returnString);
+}))
+
+// create new disc collection
+router.post("/", TryCatchAsync(async (req, res, next) =>
+{
+    const { title } = req.body
+    console.log("Someone tried to use API to post a disc collection");
+    console.log("with the title of: ", req.body)
+    const newDiscCollection = new DiscCollectionModel({
+        title,
+        discs: []
+    });
+    await newDiscCollection.save();
+    console.log("New disccollection added to db");
+    res.status(201).json(newDiscCollection);
+}));
+
+// delete route, to nuke a collection from orbit
+router.delete("/:collectionId", TryCatchAsync(async (req, res, next) =>
+{
+    const { collectionId } = req.params
+    console.log("Someone tried to use API to post a disc collection");
+    console.log(`using the param ${collectionId}`)
+    const collectionToDelete = await DiscCollectionModel.findOneAndDelete(
+        {
+            _id: collectionId
+        }
+    )
+    console.log(`Collection ${collectionToDelete} is possibly removed from DB`);
+    res.status(200).json(collectionToDelete);
+}));
+
+module.exports = router;
--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,7 @@ import
     DiscCollectionModel
 } from "./models/models"
 
+const disccollectionRoutes = require("./routes/disccollections");
 const referencedvdRoutes = require("./routes/referencedvds");
 
 // start up mongoose
@@ -42,71 +43,9 @@ app.use(express.urlencoded({ extended: false }))
 
 // routers
 app.use("/api/v1/referencedvds", referencedvdRoutes)
+app.use("/api/v1/disccollections", disccollectionRoutes)
 
-// disc collection restful routing
 
-// index a list of all disc collections (in future: only DCs that user is authorized to see)
-app.get("/api/v1/disccollections", TryCatchAsync(async (req, res, next) =>
-{
-    const allCollections = await DiscCollectionModel
-        .find({})
-        .populate({
-            path: "discs",
-            populate: {
-                path: "referenceDVD"
-            }
-        })
-        .exec();
-    const returnString = JSON.stringify(allCollections);
-    res.status(200).send(returnString);
-}))
-
-// show individual disc collection
-app.get("/api/v1/disccollections/:collectionId", TryCatchAsync(async (req, res, next) =>
-{
-    console.log(req.params.collectionId);
-    const collectionOfConcern = await DiscCollectionModel
-        .findOne({ _id: req.params.collectionId })
-        .populate({
-            path: "discs",
-            populate: {
-                path: "referenceDVD"
-            }
-        })
-        .exec();
-    const returnString = JSON.stringify(collectionOfConcern);
-    res.status(200).send(returnString);
-}))
-
-// create new disc collection
-app.post("/api/v1/disccollections", TryCatchAsync(async (req, res, next) =>
-{
-    const { title } = req.body
-    console.log("Someone tried to use API to post a disc collection");
-    console.log("with the title of: ", req.body)
-    const newDiscCollection = new DiscCollectionModel({
-        title,
-        discs: []
-    });
-    await newDiscCollection.save();
-    console.log("New disccollection added to db");
-    res.status(201).json(newDiscCollection);
-}));
-
-// delete route, to nuke a collection from orbit
-app.delete("/api/v1/disccollections/:collectionId", TryCatchAsync(async (req, res, next) =>
-{
-    const { collectionId } = req.params
-    console.log("Someone tried to use API to post a disc collection");
-    console.log(`using the param ${collectionId}`)
-    const collectionToDelete = await DiscCollectionModel.findOneAndDelete(
-        {
-            _id: collectionId
-        }
-    )
-    console.log(`Collection ${collectionToDelete} is possibly removed from DB`);
-    res.status(200).json(collectionToDelete);
-}));
 
 // dvd logic
 // add a dvd to an existing collection by dvd barcode
