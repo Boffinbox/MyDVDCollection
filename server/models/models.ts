d6fce47282c7da70265f7a728ec4823f024ae126
Boffinbox <boffinbox@hotmail.co.uk>
2023-10-15 01:35:41 +0100

    add typegoose definition for user and session schemas

--- server/models/models.ts
+++ server/models/models.ts
@@ -1,4 +1,15 @@
-import { prop, getModelForClass, Ref, setGlobalOptions, Severity, post } from "@typegoose/typegoose"
+import
+{
+    prop,
+    getModelForClass,
+    Ref,
+    setGlobalOptions,
+    Severity,
+    post,
+    plugin
+} from "@typegoose/typegoose"
+
+const passportLocalMongoose = require("passport-local-mongoose");
 
 setGlobalOptions(
     {
@@ -50,6 +61,40 @@ export class DiscCollection
     discs!: Ref<DVD>[];
 }
 
+export class Session
+{
+    @prop({ required: true, default: "" })
+    refreshToken!: string
+}
+
+const emailRegExpLiteral =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+@plugin(passportLocalMongoose)
+export class User
+{
+    @prop({ required: true, default: "" })
+    username!: string
+
+    @prop({
+        required: true,
+        default: "",
+        validate: {
+            validator: function (v: string): boolean
+            {
+                return emailRegExpLiteral.test(v)
+            },
+            message: "not a valid email"
+        }
+    })
+    email!: string
+
+    @prop({ required: true, default: "local" })
+    authStrategy!: string
+
+    @prop({ required: true, default: [], type: () => [Session] })
+    refreshToken!: Session[]
+}
 
 export const ReferenceDVDModel = getModelForClass(ReferenceDVD);
 export const DVDModel = getModelForClass(DVD);
