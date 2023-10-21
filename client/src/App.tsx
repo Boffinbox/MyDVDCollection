f5f2e3aa0bd50e278c4a4abf4e60e9cd780c684b
Boffinbox <boffinbox@hotmail.co.uk>
2023-10-21 22:13:37 +0100

    add client side login to retrieve token and refreshtoken
    does not work across https

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
 import "./App.css"
-import { TestCollectionRemove } from "./TestCollectionRemove"
 import { TestCollectionRender } from "./TestCollectionRender"
-
 import { TestCollectionSubmit } from "./TestCollectionSubmit"
 import { TestCollectionAddDVD } from "./TestCollectionAddDVD"
 import { TestDVDSubmit } from "./TestDVDSubmit"
-import { TestCollectionRemoveDVD } from "./TestCollectionRemoveDVD"
+import { TestLoginSubmit } from "./TestLoginSubmit"
 
 function App()
 {
     return (
         <div>
             <TestDVDSubmit />
+            <TestLoginSubmit />
             <TestCollectionSubmit />
             <TestCollectionAddDVD />
             <TestCollectionRender />
---
+++ client/src/TestLoginSubmit.tsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
+
+export function TestLoginSubmit()
+{
+    const [formData, setFormData] = useState({ email: "", password: "" })
+
+    function handleChange(evt: React.ChangeEvent<HTMLInputElement>)
+    {
+        setFormData(currentData =>
+        {
+            return {
+                ...currentData,
+                [evt.target.name]: evt.target.value
+            }
+        })
+    }
+
+    function handleSubmit(evt: React.FormEvent<HTMLFormElement>)
+    {
+        evt.preventDefault();
+        console.log("Form submitted!");
+        console.log("Email is: ", formData.email);
+        console.log("Password is: ", formData.password);
+        const userData = {
+            email: formData.email,
+            password: formData.password
+        }
+        axios.post(`/api/v1/users/login`, userData).then((response) =>
+        {
+            console.log("Login post request received.");
+        }).catch((e) =>
+        {
+            console.log(e);
+        })
+    }
+
+    return (
+        <div>
+            <div style={{ backgroundColor: "darkblue" }}>
+                <form action="" onSubmit={handleSubmit}>
+                    <p>form to login and get a refresh token</p>
+                    <div>
+                        <label htmlFor="email">email</label>
+                        <input type="text" id="email" name="email" onChange={handleChange} value={formData.email} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">password</label>
+                        <input type="text" id="password" name="password" onChange={handleChange} value={formData.password} />
+                    </div>
+                    <button>Submit!</button>
+                </form>
+            </div>
+        </div>
+    )
+}
