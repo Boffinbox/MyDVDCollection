5719fbec9760b17fba28a09804ada95cf7f0ea83
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-06 19:53:57 +0000

    update client refreshtoken logic to only call once on page load

--- client/src/TestLoginRefresh.tsx
+++ client/src/TestLoginRefresh.tsx
@@ -1,19 +1,23 @@
 import axios from "axios";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import { UserContext } from "./TestUserContext";
 
 export function TestLoginRefresh()
 {
     const user = useContext(UserContext);
 
-    axios.post(`/api/v1/users/refreshToken`).then((response) =>
+    useEffect(() =>
     {
-        console.log("Refresh request received.");
-        user.setToken(() => response.data.token)
-    }).catch((e) =>
-    {
-        console.log(e);
-    })
+        axios.post(`/api/v1/users/refreshToken`).then((response) =>
+        {
+            console.log("Refresh request received.");
+            user.setToken(() => response.data.token)
+        }).catch((e) =>
+        {
+            console.log(e);
+        })
+    }, []);
+
     return (
         <div>our token data is: {user.token}</div>
     )
