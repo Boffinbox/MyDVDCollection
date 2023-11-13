90f20c4ab71d8cb691f8d7425e08573238fc25fb
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-13 20:14:52 +0000

    refactor cookie methods out to cookie test helper file

---
+++ server/tests/helpers/cookies.ts
@@ -0,0 +1,33 @@
+function selectCookieStringFromArray(cookies: string[])
+{
+    return cookies[cookies.findIndex((cookie) => cookie.includes("refreshToken"))];
+}
+
+function convertCookieStringToObject(cookie: string)
+{
+    return cookie.split('; ').reduce((prev, current) =>
+    {
+        const [name, ...value] = current.split('=');
+        prev[name] = value.join('=');
+        return prev;
+    }, {});
+}
+
+function getRefreshTokenFromCookieObject(cookie): string
+{
+    const stringWeCareAbout = cookie.refreshToken;
+    const fourSliced = stringWeCareAbout.slice(4);
+    const splitString = fourSliced.split(".")
+    const theFirstThreeParts = splitString[0] + "." + splitString[1] + "." + splitString[2];
+    return theFirstThreeParts;
+}
+
+function getRefreshTokenFromResponseHeader(cookies: string[]): string
+{
+    const cookieOfInterest = selectCookieStringFromArray(cookies);
+    const cookieOfInterestObject = convertCookieStringToObject(cookieOfInterest);
+    const actualRefreshToken = getRefreshTokenFromCookieObject(cookieOfInterestObject);
+    return actualRefreshToken;
+}
+
+module.exports = getRefreshTokenFromResponseHeader;
