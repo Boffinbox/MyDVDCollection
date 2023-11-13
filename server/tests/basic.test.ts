7be393c113fb460f23aebb4686df206ba85ea23c
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-13 18:31:45 +0000

    remove describe blocks, each test should be self describing

--- server/tests/basic.test.ts
+++ server/tests/basic.test.ts
@@ -4,42 +4,33 @@ const request = require("supertest");
 const app = require("../app.ts");
 const api = "/api/v1"
 
-describe(`GET ${api}/referencedvds/testroute`, () =>
+test(`should return a json with the status message "it worked"`, async () =>
 {
-    test(`should return a json with the status message "it worked"`, async () =>
-    {
-        const res = await request(app)
-            .get(`${api}/referencedvds/testroute`);
-        expect(res.status).toBe(200);
-        expect(res.body.status).toBe("it worked");
-    })
-});
+    const res = await request(app)
+        .get(`${api}/referencedvds/testroute`);
+    expect(res.status).toBe(200);
+    expect(res.body.status).toBe("it worked");
+})
 
-describe(`GET ${api}/referencedvds/`, () =>
+test(`should return a json with empty data from test db`, async () =>
 {
-    test(`should return a json with empty data from test db`, async () =>
-    {
-        const res = await request(app)
-            .get(`${api}/referencedvds/`);
-        expect(res.status).toBe(200);
-        expect(res.body).toEqual([])
-        console.log(res.body);
-    })
-});
+    const res = await request(app)
+        .get(`${api}/referencedvds/`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([])
+    console.log(res.body);
+})
 
-describe(`POST ${api}/referencedvds/`, () =>
+test(`should add a referencedvd called "gremlins" with barcode "987654321"`, async () =>
 {
-    test(`should add a referencedvd called "gremlins" with barcode "987654321"`, async () =>
-    {
-        const res = await request(app)
-            .post(`${api}/referencedvds/`)
-            .send(
-                {
-                    title: "gremlins",
-                    barcode: "987654321"
-                }
-            );
-        expect(res.status).toBe(201);
-        expect(res.body).toMatchObject({ title: "gremlins", barcode: "987654321" })
-    })
-});
+    const res = await request(app)
+        .post(`${api}/referencedvds/`)
+        .send(
+            {
+                title: "gremlins",
+                barcode: "987654321"
+            }
+        );
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject({ title: "gremlins", barcode: "987654321" })
+})
--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -7,22 +7,36 @@ const api = "/api/v1"
 // add defines here
 const jwt = require("jsonwebtoken")
 
-describe(`POST ${api}/users/register`, () =>
+test(`add a user "boff", email "boff@test.co.uk with pass "1234"`, async () =>
 {
-    test(`should add a user "boff", email "boff@test.co.uk with pass "1234"`, async () =>
-    {
-        const res = await request(app)
-            .post(`${api}/users/register`)
-            .send(
-                {
-                    username: "boff",
-                    email: "boff@test.co.uk",
-                    password: "1234"
-                }
-            );
-        const userResult = jwt.decode(res.body.token);
-        expect(res.status).toBe(201);
-        expect(res.body.success).toBe(true);
-        expect(userResult.username).toBe("boff");
-    })
-});
+    const res = await request(app)
+        .post(`${api}/users/register`)
+        .send(
+            {
+                username: "boff",
+                email: "boff@test.co.uk",
+                password: "1234"
+            }
+        );
+    const userResult = jwt.decode(res.body.token);
+    expect(res.status).toBe(201);
+    expect(res.body.success).toBe(true);
+    expect(userResult.username).toBe("boff");
+})
+
+test(`add "boff" again, to check for test db clearance`, async () =>
+{
+    const res = await request(app)
+        .post(`${api}/users/register`)
+        .send(
+            {
+                username: "boff",
+                email: "boff@test.co.uk",
+                password: "1234"
+            }
+        );
+    const userResult = jwt.decode(res.body.token);
+    expect(res.status).toBe(201);
+    expect(res.body.success).toBe(true);
+    expect(userResult.username).toBe("boff");
+})
