bc27b542f128eb49a6771f57fc4a24ad065310cf
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-14 19:35:40 +0000

    add test to see if all known collections for a user are fetched

--- server/tests/disccollections.test.ts
+++ server/tests/disccollections.test.ts
@@ -79,3 +79,41 @@ test(`delete a known collection for a known user`, async () =>
         .send();
     expect(getResTwo.status).toBe(401);
 })
+
+test(`retrieve all collections for a known user`, async () =>
+{
+    const userDetails = userFunctions.generateUserDetails();
+    const registerRes = await userFunctions.registerAUser(userDetails);
+    const userToken = registerRes.body.token
+    expect(registerRes.status).toBe(201);
+
+    const collOne = await request(app)
+        .post(`${api}/disccollections`)
+        .set(`Authorization`, `Bearer ${userToken}`)
+        .send({ title: "My First Collection" });
+    expect(collOne.status).toBe(201);
+    expect(collOne.body.title).toEqual("My First Collection");
+
+    const collTwo = await request(app)
+        .post(`${api}/disccollections`)
+        .set(`Authorization`, `Bearer ${userToken}`)
+        .send({ title: "My Second Collection" });
+    expect(collTwo.status).toBe(201);
+    expect(collTwo.body.title).toEqual("My Second Collection");
+
+    const collThree = await request(app)
+        .post(`${api}/disccollections`)
+        .set(`Authorization`, `Bearer ${userToken}`)
+        .send({ title: "My Third Collection" });
+    expect(collThree.status).toBe(201);
+    expect(collThree.body.title).toEqual("My Third Collection");
+
+    const res = await request(app)
+        .get(`${api}/disccollections`)
+        .set(`Authorization`, `Bearer ${userToken}`)
+        .send();
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0].title).toEqual("My First Collection");
+    expect(res.body[1].title).toEqual("My Second Collection");
+    expect(res.body[2].title).toEqual("My Third Collection");
+})
