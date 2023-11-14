49b1fb75212c88c49f654f6160f78e5a384cad5c
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-14 18:48:33 +0000

    refactor user test functions out to user test helper

--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -6,44 +6,13 @@ const api = "/api/v1"
 
 // add defines here
 const jwt = require("jsonwebtoken")
-const cookieFunctions = require("./helpers/cookies.ts");
-
-// interfaces
-interface IUserDetails
-{
-    username: string;
-    email: string;
-    password: string;
-}
-
-// add functions here
-// generate user details, if none provided, returns a "boff" user details
-function generateUserDetails(username = "boff", email = "boff@test.co.uk", password = "1234")
-{
-    const userDetails: IUserDetails = { username, email, password }
-    return userDetails
-}
-
-async function registerAUser(userDetails: IUserDetails)
-{
-    const res = await request(app)
-        .post(`${api}/users/register`)
-        .send(userDetails);
-    return res;
-}
-
-async function loginAUser(userDetails: IUserDetails)
-{
-    const res = await request(app)
-        .post(`${api}/users/login`)
-        .send(userDetails);
-    return res;
-}
+const cookieFunctions = require("./helpers/cookies");
+const userFunctions = require("./helpers/users")
 
 test(`add a user with username, email and password`, async () =>
 {
-    const userDetails = generateUserDetails();
-    const res = await registerAUser(userDetails);
+    const userDetails = userFunctions.generateUserDetails();
+    const res = await userFunctions.registerAUser(userDetails);
     const userTokenPayload = jwt.verify(res.body.token, process.env.JWT_SECRET);
     expect(res.status).toBe(201);
     expect(userTokenPayload.username).toEqual(userDetails.username);
@@ -51,8 +20,8 @@ test(`add a user with username, email and password`, async () =>
 
 test(`duplicate test, to check for correct test cleaning`, async () =>
 {
-    const userDetails = generateUserDetails();
-    const res = await registerAUser(userDetails);
+    const userDetails = userFunctions.generateUserDetails();
+    const res = await userFunctions.registerAUser(userDetails);
     const userTokenPayload = jwt.verify(res.body.token, process.env.JWT_SECRET);
     expect(res.status).toBe(201);
     expect(userTokenPayload.username).toEqual(userDetails.username);
@@ -60,9 +29,9 @@ test(`duplicate test, to check for correct test cleaning`, async () =>
 
 test(`login using a registered user's details`, async () =>
 {
-    const userDetails = generateUserDetails();
-    await registerAUser(userDetails);
-    const res = await loginAUser(userDetails);
+    const userDetails = userFunctions.generateUserDetails();
+    await userFunctions.registerAUser(userDetails);
+    const res = await userFunctions.loginAUser(userDetails);
     const userTokenPayload = jwt.verify(res.body.token, process.env.JWT_SECRET);
     const refreshToken = cookieFunctions.getRefreshTokenFromResponseHeader(res.headers["set-cookie"]);
     const refreshTokenPayload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
@@ -73,9 +42,9 @@ test(`login using a registered user's details`, async () =>
 
 test(`refresh a refresh token and check refresh count has increased`, async () =>
 {
-    const userDetails = generateUserDetails();
-    await registerAUser(userDetails);
-    const reqOne = await loginAUser(userDetails);
+    const userDetails = userFunctions.generateUserDetails();
+    await userFunctions.registerAUser(userDetails);
+    const reqOne = await userFunctions.loginAUser(userDetails);
     const refreshTokenOneCookie = cookieFunctions.getRefreshTokenCookieFromResponseHeader(reqOne.headers["set-cookie"])
     const refreshTokenOne = cookieFunctions.getRefreshTokenFromResponseHeader(reqOne.headers["set-cookie"]);
     const refreshTokenOneDecoded = jwt.verify(refreshTokenOne, process.env.REFRESH_TOKEN_SECRET);
@@ -91,9 +60,9 @@ test(`refresh a refresh token and check refresh count has increased`, async () =
 
 test(`make a user, login, then logout, then try to misuse the invalid refresh token`, async () =>
 {
-    const userDetails = generateUserDetails();
-    await registerAUser(userDetails);
-    const reqOne = await loginAUser(userDetails);
+    const userDetails = userFunctions.generateUserDetails();
+    await userFunctions.registerAUser(userDetails);
+    const reqOne = await userFunctions.loginAUser(userDetails);
     const refreshTokenCookie = cookieFunctions.getRefreshTokenCookieFromResponseHeader(reqOne.headers["set-cookie"])
     const logout = await request(app)
         .post(`${api}/users/logout`)
