d0f286a2c5ad027eee83952377bde85495ff91a3
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-14 18:48:15 +0000

    add typing for reference dvd tests

--- server/tests/referencedvds.test.ts
+++ server/tests/referencedvds.test.ts
@@ -10,28 +10,28 @@ const jwt = require("jsonwebtoken")
 // interfaces
 interface IReferenceDVDDetails
 {
-    username: string;
-    email: string;
-    password: string;
+    title: string;
+    barcode: string;
 }
 
 function generateReferenceDVDDetails(title = "gremlins", barcode = "0883929003518")
 {
-    const referenceDVDDetails = { title, barcode };
+    const referenceDVDDetails: IReferenceDVDDetails = { title, barcode };
     return referenceDVDDetails
 }
 
-async function addAReferenceDVD(title = "gremlins", barcode = "0883929003518")
+async function addAReferenceDVD(referenceDVDDetails: IReferenceDVDDetails)
 {
     const res = await request(app)
         .post(`${api}/referencedvds/`)
-        .send({ title, barcode });
+        .send(referenceDVDDetails);
     return res;
 }
+
 test(`add a reference dvd`, async () =>
 {
     const dvdDetails = generateReferenceDVDDetails();
-    const res = await addAReferenceDVD();
+    const res = await addAReferenceDVD(dvdDetails);
     expect(res.status).toBe(201);
     expect(res.body).toMatchObject(dvdDetails)
 })
