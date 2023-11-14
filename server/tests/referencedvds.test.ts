b10703000682e6051950b661bc9031b524fb77a1
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-14 18:54:22 +0000

    refactor referencedvd test functions out to referencedvd helper

--- server/tests/referencedvds.test.ts
+++ server/tests/referencedvds.test.ts
@@ -6,32 +6,12 @@ const api = "/api/v1"
 
 // add defines here
 const jwt = require("jsonwebtoken")
-
-// interfaces
-interface IReferenceDVDDetails
-{
-    title: string;
-    barcode: string;
-}
-
-function generateReferenceDVDDetails(title = "gremlins", barcode = "0883929003518")
-{
-    const referenceDVDDetails: IReferenceDVDDetails = { title, barcode };
-    return referenceDVDDetails
-}
-
-async function addAReferenceDVD(referenceDVDDetails: IReferenceDVDDetails)
-{
-    const res = await request(app)
-        .post(`${api}/referencedvds/`)
-        .send(referenceDVDDetails);
-    return res;
-}
+const referencedvdFunctions = require("./helpers/referencedvds")
 
 test(`add a reference dvd`, async () =>
 {
-    const dvdDetails = generateReferenceDVDDetails();
-    const res = await addAReferenceDVD(dvdDetails);
+    const dvdDetails = referencedvdFunctions.generateReferenceDVDDetails();
+    const res = await referencedvdFunctions.addAReferenceDVD(dvdDetails);
     expect(res.status).toBe(201);
     expect(res.body).toMatchObject(dvdDetails)
 })
