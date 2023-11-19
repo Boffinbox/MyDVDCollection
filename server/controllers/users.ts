a010364168c6c823df77bba59c034725dae90eb4
Boffinbox <boffinbox@hotmail.co.uk>
2023-11-19 02:34:25 +0000

    refactor user token logic out to reusable functions

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -5,16 +5,36 @@ const { UserModel } = require("../models")
 
 const getUserDocument = require("../helpers/GetUserDocument");
 
+async function getTokens(user)
+{
+    const jwt = getToken({ _id: user._id, username: user.username })
+    const refreshToken = await getRefreshToken({ _id: user._id, username: user.username, refreshCount: 0 })
+    return { jwt, refreshToken };
+}
+
+function addNewRefreshTokenToUser(user, refreshToken)
+{
+    user.refreshTokens.push({ refreshToken, refreshCount: 0 })
+    return user;
+}
+
+function updateExistingRefreshToken(user, tokenIndex: number)
+{
+    const newRefreshCount = user.refreshTokens[tokenIndex].refreshCount + 1
+    const refreshToken = getRefreshToken({ _id: user._id, username: user.username, refreshCount: newRefreshCount });
+    user.refreshTokens[tokenIndex] = { refreshToken, refreshCount: newRefreshCount }
+    return { user, refreshToken };
+}
+
 export async function register(req, res)
 {
     const { username, email, password }: { username: string, email: string, password: string } = req.body;
     const newUser = await UserModel.register(new UserModel({ username, email }), password)
-    const token = await getToken({ _id: newUser._id, username: newUser.username })
-    const refreshToken = await getRefreshToken({ _id: newUser._id, username: newUser.username, refreshCount: 0 })
-    await newUser.refreshTokens.push({ refreshToken, refreshCount: 0 })
-    newUser.save().then((user) =>
+    const userTokens = await getTokens(newUser);
+    const user = addNewRefreshTokenToUser(newUser, userTokens.refreshToken);
+    user.save().then((user) =>
     {
-        return res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS).status(201).json({ success: true, token })
+        return res.cookie("refreshToken", userTokens.refreshToken, COOKIE_OPTIONS).status(201).json({ success: true, token: userTokens.jwt })
     }
     ).catch((err) =>
     {
@@ -24,17 +44,15 @@ export async function register(req, res)
 
 export async function login(req, res)
 {
-    const user = await getUserDocument(req, res);
-    const token = await getToken({ _id: user._id, username: user.username })
-    const refreshToken = await getRefreshToken({ _id: user._id, username: user.username, refreshCount: 0 })
-    user.refreshTokens.push({ refreshToken, refreshCount: 0 })
+    const newUser = await getUserDocument(req, res);
+    const userTokens = await getTokens(newUser);
+    const user = addNewRefreshTokenToUser(newUser, userTokens.refreshToken);
     user.save().then((user) =>
     {
-        return res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS).send({ success: true, token })
+        return res.cookie("refreshToken", userTokens.refreshToken, COOKIE_OPTIONS).status(200).send({ success: true, token: userTokens.jwt })
     }
     ).catch((err) =>
     {
-        console.log("hit failure route");
         return res.status(500).json(err);
     })
 }
@@ -43,18 +61,15 @@ export async function refreshToken(req, res)
 {
     const { signedCookies = {} } = req;
     const { refreshToken } = signedCookies;
-
     if (!refreshToken)
     {
         return res.status(401).send("Unauthorized");
     }
+
     const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
-    const userId = payload._id;
-    const user = await UserModel.findById({ _id: userId });
-    if (!user)
-    {
-        return res.status(401).send("Unauthorized");
-    }
+    const user = await getUserDocument(req, res, payload._id);
+
+    // check that the provided token from cookies matches one from database
     const tokenIndex = user.refreshTokens.findIndex(
         item => item.refreshToken === refreshToken
     )
@@ -62,13 +77,13 @@ export async function refreshToken(req, res)
     {
         return res.status(401).send("Unauthorized");
     }
-    const newRefreshCount = user.refreshTokens[tokenIndex].refreshCount + 1
-    const token = getToken({ _id: userId });
-    const newRefreshToken = getRefreshToken({ _id: userId, username: user.username, refreshCount: newRefreshCount });
-    user.refreshTokens[tokenIndex] = { refreshToken: newRefreshToken, refreshCount: newRefreshCount }
-    user.save().then((user) =>
+
+    const userTokens = await getTokens(user);
+    const refreshed = updateExistingRefreshToken(user, tokenIndex)
+
+    refreshed.user.save().then((user) =>
     {
-        return res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS).send({ success: true, token })
+        return res.cookie("refreshToken", refreshed.refreshToken, COOKIE_OPTIONS).send({ success: true, token: userTokens.jwt })
     }
     ).catch((err) =>
     {
