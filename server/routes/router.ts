832fb547a3f6d8c9373f20eac321c87e82e761a1
Boffinbox <boffinbox@hotmail.co.uk>
2023-12-06 19:58:18 +0000

    add collId param validation to userdvd routes

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -8,9 +8,11 @@ const referenceDVDRoutes = require("./referencedvds");
 const userDVDRoutes = require("./userdvds");
 const userRoutes = require("./users");
 
+const validateCollectionId = require("../validators/collectionId");
+
 router.use("/users", userRoutes)
 router.use("/referencedvds", referenceDVDRoutes)
 router.use("/disccollections", disccollectionRoutes)
-router.use("/disccollections/:collectionId/userdvds", userDVDRoutes)
+router.use("/disccollections/:collectionId/userdvds", validateCollectionId, userDVDRoutes)
 
 module.exports = router;
