f8d814608ff67ab1fb2922e29292c2287f8c5d49
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-08 17:14:05 +0000

    update root routes into jsx elements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import Root from './routes/root'
 import ErrorPage from './components/ErrorPage'
 // route components go here
 import Collections from './routes/collections'
 import Login from './routes/login'
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "login",
-                element: <Login />
-            },
-            {
-                path: "collections",
-                element: <Collections />
-            }
-        ],
-    }
-])
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Root />}>
+            <Route path="login" element={<Login />} />
+            <Route path="collections" element={<Collections />} />
+            <Route path="*" element={<ErrorPage />} />
+        </Route>
+    ))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
