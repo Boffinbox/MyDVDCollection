82a12763c3e238cb8b59fa69221c510c1e509268
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-15 17:50:11 +0000

    add component to allow tanstack router devtools in development

---
+++ client/src/dev/TanStackRouterDevtools.tsx
@@ -0,0 +1,16 @@
+import { lazy } from 'react'
+
+// Blatantly copied and pasted from TanStack guide
+export default function TanStackRouterDevtools()
+{
+    process.env.NODE_ENV === 'production'
+        ? () => null // Render nothing in production
+        : lazy(() =>
+            // Lazy load in development
+            import('@tanstack/router-devtools').then((res) => ({
+                default: res.TanStackRouterDevtools,
+                // For Embedded Mode
+                // default: res.TanStackRouterDevtoolsPanel
+            })),
+        )
+}
