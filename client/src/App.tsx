ba5d7bbf2039d78343efa7ea22e8326dcf60e3c6
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-15 18:28:30 +0000

    add tanstack router boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,38 @@
-import { Link, Route, Router, Switch, useRouter } from "wouter";
 import { UserContext } from "./components/UserContext"
 import { useState } from "react";
 import LoginRefresh from "./components/LoginRefresh";
-// route components go here
-import ErrorPage from './components/ErrorPage'
-import Root from './routes/root'
-import Collections from './routes/collections'
-import Login from './routes/login'
-import TestNest from "./oldtest/TestNest";
+import Login from "./routes/login";
+import Root from "./routes/root";
+// tanstack router
+import { RootRoute, Route, Router } from "@tanstack/react-router";
 
+const rootRoute = new RootRoute({ component: Root, });
+const indexRoute = new Route({ getParentRoute: () => rootRoute, path: "/" })
+const loginRoute = new Route({ getParentRoute: () => rootRoute, path: "login" })
 
-export function App()
-{
-    const [userToken, setUserToken] = useState<string>("");
-    // return <div>
-    //     <UserContext.Provider value={{ userToken, setUserToken }}>
-    //         <LoginRefresh />
+const routeTree = rootRoute.addChildren([
+    indexRoute,
+    loginRoute,
+])
 
-    //         <nav className="navbar">
-    //             <Link href="/">Root</Link>
-    //             <Link href="/login">Login</Link>
-    //             <Link href="/collections">Collections</Link>
-    //         </nav>
+const router = new Router({ routeTree })
 
-    //         <Switch>
-    //             <Route path="/" component={Root} />
-    //             <Route path="/login" component={Login} />
-    //             <Route path="/collections" component={Collections} />
-    //             <Route component={ErrorPage} />
-    //         </Switch>
-    //     </UserContext.Provider>
-    // </div>
-    const router = useRouter();
-    return <div>
-        Main Page
-        <Router base="/app">
-            <Route path="/home">Home</Route>
-            <TestNest router={router} />
-        </Router>
-    </div>
+declare module '@tanstack/react-router' {
+    interface Register
+    {
+        router: typeof router
+    }
 }
+
+// export function App()
+// {
+//     const [userToken, setUserToken] = useState<string>("");
+
+//     return <div>
+//         <RouterProvider router={router} />
+//         <UserContext.Provider value={{ userToken, setUserToken }}>
+//             <LoginRefresh />
+//             <Login />
+//         </UserContext.Provider>
+//     </div >
+// }
--- client/src/components/ErrorPage.tsx
+++ client/src/components/ErrorPage.tsx
@@ -1,12 +1,9 @@
-import { Link } from "wouter";
-
 export default function ErrorPage()
 {
     return (
         <div id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
-            <Link href="/">Go back to main page.</Link>
         </div>
     );
 }
