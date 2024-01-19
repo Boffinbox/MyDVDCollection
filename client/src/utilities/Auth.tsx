2336fb4e51c7a5578420ae1163f47cdc670ff536
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-19 19:50:21 +0000

    add auth utility, in the hope i can add it to router context

---
+++ client/src/utilities/Auth.tsx
@@ -0,0 +1,48 @@
+import axios from "axios"
+
+const login = async (email: string, password: string): Promise<string | undefined> =>
+{
+    const userData = { email, password }
+    let token: string | undefined = undefined;
+    await axios.post(`/api/v1/users/login`, userData).then((response) =>
+    {
+        console.log("Login post request received.");
+        token = response.data.token;
+    }).catch((e) =>
+    {
+        console.log(e);;
+    })
+    return token;
+}
+
+export interface IAuth
+{
+    status: "loggedOut" | "loggedIn";
+    token?: string | undefined;
+    login: (email: string, password: string) => void;
+    logout: () => void;
+}
+
+export const auth: IAuth = {
+    status: "loggedOut",
+    token: undefined,
+    login: async function (email, password)
+    {
+        const token = await login(email, password);
+        if (token != undefined)
+        {
+            auth.token = token;
+            auth.status = "loggedIn"
+        }
+        else
+        {
+            auth.status = "loggedOut"
+        }
+    },
+    logout: function ()
+    {
+        auth.token = undefined;
+        auth.status = "loggedOut"
+    }
+}
+
