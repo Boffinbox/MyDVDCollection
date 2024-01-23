61ab0ab47546a7108e9cde91cf8078a14b9efaeb
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-23 18:23:20 +0000

    remove app.tsx, refactor it into root route

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,9 +1,37 @@
 import { rootRouteWithContext } from '@tanstack/react-router'
-import { App } from '../App';
 import { IAuth } from '../utilities/Auth';
+
+import { Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = rootRouteWithContext<{
     auth: IAuth
 }>()({
     component: App
 })
+
+function App()
+{
+    const { token, status } = Route.useRouteContext({ select: ({ auth }) => ({ token: auth.token, status: auth.status }) })
+
+    return <div>
+        <p>Current token is: {token}</p>
+        <p>Status: {status}</p>
+        <hr />
+        <h1>My DVD Collection</h1>
+        <div className="p-2 flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+                Home
+            </Link>{` `}
+            <Link to="/login" className="[&.active]:font-bold">
+                Login
+            </Link>{` `}
+            <Link to="/collections" className="[&.active]:font-bold">
+                Collections
+            </Link>
+        </div>
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+    </div >
+}
--- client/src/routes/index.component.tsx
+++ client/src/routes/index.component.tsx
@@ -2,22 +2,8 @@ export const component = function Index()
 {
     return (
         <>
-            <div id="navbar">
-                <h1>My DVD Collection</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search My Collections"
-                            placeholder="Search My Collections"
-                            type="search"
-                            name="q"
-                        />
-                    </form>
-                    <form method="post">
-                        <button type="submit">New</button>
-                    </form>
-                </div>
+            <div>
+                Welcome to My DVD Collection! Your place for digitally tracking your DVDs, Blu-Rays, CDs, and more!
             </div>
         </>
     )
