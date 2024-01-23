3f59fa1777df3e94041e29db5e205631f85bb654
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-23 19:15:25 +0000

    refactor individual collection fetch out to fetch folder

--- client/src/routes/collections.$collectionId.tsx
+++ client/src/routes/collections.$collectionId.tsx
@@ -1,4 +1,5 @@
 import { FileRoute } from "@tanstack/react-router"
+import { FetchCollection } from "../fetch/FetchCollection";
 
 export const Route = new FileRoute('/collections/$collectionId').createRoute({
     loader: async ({ params: { collectionId }, context: { auth } }) => FetchCollection(collectionId, auth.token),
@@ -7,7 +8,6 @@ export const Route = new FileRoute('/collections/$collectionId').createRoute({
 
 function Collection()
 {
-    // const user = useContext(UserContext);
     const data: [] = Route.useLoaderData();
     console.log(data)
 
@@ -20,29 +20,4 @@ function Collection()
             </div>
         </>
     )
-}
-
-const FetchCollection = async function (collectionId: string, token: string | undefined)
-{
-    if (token == undefined)
-    {
-        throw new Error("No access token supplied to fetch collection.");
-    }
-    const config =
-    {
-        headers: { Authorization: `Bearer ${token}` }
-        // headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTU3YTMwZGQ1ZTU0NDQxYjE1YjAyNDQiLCJ1c2VybmFtZSI6ImJvZmYiLCJpYXQiOjE3MDU2ODgwODQsImV4cCI6MTcwNTY4ODk4NH0.KX4OX42b_YWbSl9_qDL2CJM_kxZ7OxcmEjSjv7zia9g` }
-    }
-    // axios.get(`/api/v1/disccollections/${collectionId}`, config).then((response) =>
-    // {
-    //     console.log("Collection request received.");
-    //     console.log(response.data);
-    //     return response.data
-    // }).catch((e) =>
-    // {
-    //     console.log(e);
-    // })
-    const res = await fetch(`/api/v1/disccollections/${collectionId}`, config)
-    if (!res.ok) throw new Error("Failed to fetch user's collection.")
-    return res.json()
 }
