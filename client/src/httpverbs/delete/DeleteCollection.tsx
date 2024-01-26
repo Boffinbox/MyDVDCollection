b12b55080292bb15cbba21149b3c8f187830a117
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-26 22:23:46 +0000

    add reactive collection deletion

---
+++ client/src/httpverbs/delete/DeleteCollection.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export async function DeleteCollection(token: string | undefined, collectionId: string)
+{
+    if (token == undefined)
+    {
+        throw new Error("No access token supplied to delete collection.");
+    }
+    const config =
+    {
+        headers: { Authorization: `Bearer ${token}` }
+    }
+    try
+    {
+        const response = await axios.delete(`/api/v1/disccollections/${collectionId}`, config)
+        console.log("token used was: ", token);
+        console.log("Deletion request received.");
+        console.log(response.data.message)
+    }
+    catch (e)
+    {
+        throw new Error(`Failed to delete collection ${collectionId}.`);
+    }
+}
--- client/src/routes/_mdc/collections.tsx
+++ client/src/routes/_mdc/collections.tsx
@@ -1,5 +1,7 @@
-import { Link, Outlet, FileRoute } from "@tanstack/react-router"
+import { Link, Outlet, FileRoute, useRouter } from "@tanstack/react-router"
 import { GetCollections } from "../../httpverbs/get/GetCollections"
+import { DeleteCollection } from "../../httpverbs/delete/DeleteCollection";
+import { DeleteButton } from "../../components/DeleteButton";
 
 export const Route = new FileRoute('/_mdc/collections').createRoute({
     loader: async ({ context: { auth } }) => await GetCollections(auth.token),
@@ -8,6 +10,10 @@ export const Route = new FileRoute('/_mdc/collections').createRoute({
 
 function Collections()
 {
+    const { token } = Route.useRouteContext({ select: ({ auth }) => ({ token: auth.token }) })
+
+    const router = useRouter();
+
     const data: [{ _id: string, title: string }] = Route.useLoaderData();
     console.log("My coll data is: ", data)
 
@@ -26,9 +32,10 @@ function Collections()
                             Click to load the "{coll.title} collection".
                         </Link>
                         {` `}
-                        <button>
-                            Delete!
-                        </button>
+                        <DeleteButton
+                            deleteFromServer={async () => await DeleteCollection(token, coll._id)}
+                            deleteFromClient={() => router.invalidate()}
+                        />
                     </div>
                 ))}
                 <hr />
