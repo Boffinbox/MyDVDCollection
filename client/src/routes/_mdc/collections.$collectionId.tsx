06718ab5e4d60baf8db88e31d6813c8a8fa4a4e7
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-26 21:47:51 +0000

    add stateful delete buttons for individual client side dvds

--- client/src/routes/_mdc/collections.$collectionId.tsx
+++ client/src/routes/_mdc/collections.$collectionId.tsx
@@ -3,6 +3,7 @@ import { GetCollection } from "../../httpverbs/get/GetCollection";
 import { DeleteDisc } from "../../httpverbs/delete/DeleteDisc";
 import { PostBarcode } from "../../httpverbs/post/PostBarcode";
 import { useState } from "react";
+import { DeleteButton } from "../../components/DeleteButton";
 
 export const Route = new FileRoute('/_mdc/collections/$collectionId').createRoute({
     loader: async ({ params: { collectionId }, context: { auth } }) => GetCollection(collectionId, auth.token),
@@ -14,23 +15,25 @@ function Collection()
     const { token } = Route.useRouteContext({ select: ({ auth }) => ({ token: auth.token }) })
     const [formData, setFormData] = useState({ barcode: "" })
 
-    const data: {
+    interface ICollData
+    {
         _id: string,
         title: string,
-        discs: [
+        discs:
+        {
+            _id: string,
+            rating: number,
+            watched: boolean,
+            referenceDVD:
             {
                 _id: string,
-                rating: number,
-                watched: boolean,
-                referenceDVD:
-                {
-                    _id: string,
-                    barcode: string,
-                    title: string
-                }
+                barcode: string,
+                title: string
             }
-        ]
-    } = Route.useLoaderData();
+        }[]
+    }
+
+    const [collData, setCollData] = useState<ICollData>(Route.useLoaderData())
 
     function handleChange(evt: React.ChangeEvent<HTMLInputElement>)
     {
@@ -45,13 +48,13 @@ function Collection()
 
     return (
         <>
-            <h3>{data.title}</h3>
+            <h3>{collData.title}</h3>
             <form action="" onSubmit={async (evt) => 
             {
                 evt.preventDefault();
                 try
                 {
-                    await PostBarcode(token, data._id, formData.barcode)
+                    await PostBarcode(token, collData._id, formData.barcode)
                     setFormData(() => { return { barcode: "" } })
                 }
                 catch (e)
@@ -69,26 +72,24 @@ function Collection()
                 </p>
             </form>
             <div>
-                {data.discs.map((disc, idx) => (
+                {collData.discs.map((disc, idx) => (
                     <div key={disc._id}>
-                        <form action="" onSubmit={async (evt) =>
-                        {
-                            evt.preventDefault();
-                            try
+                        Disc {idx + 1}: {disc.referenceDVD.title}
+                        {` `}
+                        <DeleteButton
+                            deleteFromServer={async () => await DeleteDisc(token, collData._id, disc._id)}
+                            deleteFromClient={() => 
                             {
-                                await DeleteDisc(token, data._id, disc._id)
-                            }
-                            catch (e)
-                            {
-                                console.log(e);
-                            }
-                        }}>
-                            Disc {idx + 1}: {disc.referenceDVD.title}
-                            {` `}
-                            <button>
-                                Delete!
-                            </button>
-                        </form>
+                                const newDiscs = collData.discs.filter((item) => item._id !== disc._id)
+                                setCollData((prevData) =>
+                                {
+                                    return {
+                                        ...prevData,
+                                        discs: newDiscs
+                                    }
+                                })
+                            }}
+                        />
                     </div>
                 ))}
             </div >
