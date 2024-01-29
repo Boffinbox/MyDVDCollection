41b5779a0dce18eb6c40f90599673f233e743228
Boffinbox <boffinbox@hotmail.co.uk>
2024-01-29 15:42:09 +0000

    add collection data query using tanstack query

--- client/src/queries/Collections.tsx
+++ client/src/queries/Collections.tsx
@@ -1,9 +1,9 @@
 import { queryOptions } from "@tanstack/react-query"
 import { GetCollections } from "../httpverbs/get/GetCollections"
 
-export function CollectionsQueryOptions(token: string)
+export function CollectionsQueryOptions(token: string | undefined)
 {
-    queryOptions({
+    return queryOptions({
         queryKey: ["collections"],
         queryFn: () => GetCollections(token)
     })
--- client/src/routes/_mdc/collections.tsx
+++ client/src/routes/_mdc/collections.tsx
@@ -1,12 +1,21 @@
 import { Link, Outlet, createFileRoute, useRouter } from "@tanstack/react-router"
+import { useQuery, useMutation, useSuspenseQuery } from "@tanstack/react-query";
+
 import { GetCollections } from "../../httpverbs/get/GetCollections"
 import { DeleteCollection } from "../../httpverbs/delete/DeleteCollection";
 import { PostCollection } from "../../httpverbs/post/PostCollection";
+
 import { StateChangingButton } from "../../components/StateChangingButton";
 import { SingleLineForm } from "../../components/SingleLineForm";
 
+import { CollectionsQueryOptions } from "../../queries/Collections"
+import { Suspense } from "react";
+
 export const Route = createFileRoute('/_mdc/collections')({
-    loader: async ({ context: { auth } }) => await GetCollections(auth.token),
+    loader: async ({ context: { auth, queryClient } }) =>
+    {
+        await queryClient.ensureQueryData(CollectionsQueryOptions(auth.token))
+    },
     component: Collections
 })
 
@@ -16,43 +25,46 @@ function Collections()
 
     const router = useRouter();
 
-    const data: [{ _id: string, title: string }] = Route.useLoaderData();
+    const collectionsQuery = useSuspenseQuery(CollectionsQueryOptions(token))
+    const collections: [{ _id: string, title: string }] = collectionsQuery.data;
 
     return (
         <>
-            <SingleLineForm
-                submitButtonText="Submit!"
-                labelText="Title"
-                toServer={async (title) => await PostCollection(token, title)}
-                toClient={() => 
-                {
-                    console.log("invalidating router...")
-                    router.invalidate()
-                }}
-            />
-            <div>
-                Collections {` `}
-                {data.map((coll) => (
-                    <div key={coll._id}>
-                        <Link
-                            to="/collections/$collectionId"
-                            params={{
-                                collectionId: coll._id
-                            }}
-                        >
-                            Click to load the "{coll.title} collection".
-                        </Link>
-                        {` `}
-                        <StateChangingButton
-                            text={"Delete..."}
-                            toServer={async () => await DeleteCollection(token, coll._id)}
-                            toClient={() => router.invalidate()}
-                        />
-                    </div>
-                ))}
-                <hr />
-                <Outlet />
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <SingleLineForm
+                    submitButtonText="Submit!"
+                    labelText="Title"
+                    toServer={async (title) => await PostCollection(token, title)}
+                    toClient={() => 
+                    {
+                        console.log("invalidating router...")
+                        router.invalidate()
+                    }}
+                />
+                <div>
+                    Collections {` `}
+                    {collections.map((coll) => (
+                        <div key={coll._id}>
+                            <Link
+                                to="/collections/$collectionId"
+                                params={{
+                                    collectionId: coll._id
+                                }}
+                            >
+                                Click to load the "{coll.title} collection".
+                            </Link>
+                            {` `}
+                            <StateChangingButton
+                                text={"Delete..."}
+                                toServer={async () => await DeleteCollection(token, coll._id)}
+                                toClient={() => router.invalidate()}
+                            />
+                        </div>
+                    ))}
+                    <hr />
+                    <Outlet />
+                </div>
+            </Suspense>
         </>
     )
 }
