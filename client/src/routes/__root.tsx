205d9c3e66a4456e04bf123fb28555640f8ccc06
Boffinbox <boffinbox@hotmail.co.uk>
2024-02-02 17:58:59 +0000

    add very basic style for the whole app

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,11 @@
-import { CssVarsProvider } from '@mui/joy/styles';
-import CssBaseline from '@mui/joy/CssBaseline';
-
 import { rootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { QueryClient } from '@tanstack/react-query';
 
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+import { CssVarsProvider } from '@mui/joy/styles';
+import { CssBaseline, Sheet } from '@mui/joy';
 
 export const Route = rootRouteWithContext<{
     queryClient: QueryClient
@@ -18,7 +18,23 @@ function App()
     return <div>
         <CssVarsProvider>
             <CssBaseline>
-                <Outlet />
+                <Sheet
+                    variant='outlined'
+                    sx={{
+                        width: 600,
+                        mx: 'auto', // margin left & right
+                        my: 4, // margin top & bottom
+                        py: 3, // padding top & bottom
+                        px: 2, // padding left & right
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                        borderRadius: 'sm',
+                        boxShadow: 'md',
+                    }}
+                >
+                    <Outlet />
+                </Sheet>
             </CssBaseline>
         </CssVarsProvider>
         <ReactQueryDevtools buttonPosition='top-right' position='right' />
