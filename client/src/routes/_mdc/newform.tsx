af109b4a2bb0759310a9dc3bdb1100af4719f444
Boffinbox <boffinbox@hotmail.co.uk>
2024-02-09 18:44:40 +0000

    add barcode input field for new disc form

--- client/src/routes/_mdc/newform.tsx
+++ client/src/routes/_mdc/newform.tsx
@@ -3,7 +3,7 @@ import { createFileRoute } from "@tanstack/react-router"
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AccessTokenQueryOptions, CollectionsQueryOptions } from "../../utilities/Queries";
 
-import { Divider, Select, Option, Typography, FormControl, FormLabel, FormHelperText, Stack, Button } from "@mui/joy"
+import { Divider, Select, Option, Typography, FormControl, FormLabel, FormHelperText, Stack, Button, Input } from "@mui/joy"
 
 export const Route = createFileRoute('/_mdc/newform')({
     component: NewForm
@@ -19,39 +19,61 @@ function NewForm()
     const collectionsQuery = useQuery(CollectionsQueryOptions(token))
     const collections: [{ _id: string, title: string }] = collectionsQuery.data;
 
+    function handleSubmit(evt: React.ChangeEvent<HTMLFormElement>)
+    {
+        evt.preventDefault();
+        const formData = new FormData(evt.currentTarget);
+        const formJson = Object.fromEntries((formData as any).entries());
+        console.log("collId: ", formJson.collId, " barcode: ", formJson.barcode);
+    }
+
     return (
         <>
-            <Typography level="h1">Add a DVD</Typography>
-            <Divider />
-            <form
-                onSubmit={(event) =>
-                {
-                    event.preventDefault();
-                    const formData = new FormData(event.currentTarget);
-                    const formJson = Object.fromEntries((formData as any).entries());
-                    console.log(formJson.collId);
-                }}
-            >
-                <FormControl>
-                    <FormLabel>
-                        Choose collection to add to:
-                    </FormLabel>
-                    <Select
-                        placeholder="Select a collection"
-                        name="collId"
-                        required
-                    >
-                        <Option value="dog">Dog</Option>
-                        <Option value="cat">Cat</Option>
-                        <Option value="pp">Fish</Option>
-                        <Option value="bird">Bird</Option>
-                    </Select>
-                    <FormHelperText>
-                        This is a helper text.
-                    </FormHelperText>
-                    <Button type="submit">Submit</Button>
-                </FormControl>
-            </form>
+            <Stack gap={1}>
+                <Typography level="h1">Add a Disc</Typography>
+                <Divider />
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <Stack gap={3}>
+                        <FormControl>
+                            <FormLabel>
+                                Choose collection to add to:
+                            </FormLabel>
+                            <Select
+                                placeholder="Select a collection"
+                                name="collId"
+                                required
+                            >
+                                {collections.map((coll) => (
+                                    <Option
+                                        key={coll._id}
+                                        value={coll._id}
+                                    >
+                                        {coll.title}</Option>
+                                ))}
+                            </Select>
+                            <FormHelperText>
+                                $TODO helper text collId
+                            </FormHelperText>
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>
+                                Barcode
+                            </FormLabel>
+                            <Input
+                                placeholder="Type barcode here"
+                                name="barcode"
+                                required
+                            />
+                            <FormHelperText>
+                                $TODO helper text barcode
+                            </FormHelperText>
+                        </FormControl>
+                        <Button type="submit">Add Disc</Button>
+                    </Stack>
+                </form>
+            </Stack>
         </>
     )
 
