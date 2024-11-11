4a0845d3c0ccfe1e034b96975c3fadd6d5229284
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2024-11-11 17:28:47 +0000

    update checkmark css mui emotion animation, it's almost there

--- client/src/components/Checkmark.tsx
+++ client/src/components/Checkmark.tsx
@@ -5,46 +5,88 @@ export function Checkmark()
 {
     const spin = keyframes`
     0% {
+        transform: rotate(180deg);
+    }
+    1% {
         transform: rotate(0deg);
     }
     100%
     {
-        transform: rotate(360deg);
+        transform: rotate(180deg);
     }`;
 
     return (
         <>
             <Sheet sx={{
-                display: "inline-block",
+                position: "relative",
                 height: "20dvh",
-                width: "20dvh",
-
+                width: "20dvh"
             }}>
                 <Sheet sx={{
+                    position: "absolute",
                     height: "100%",
                     width: "100%",
-                    border: "1.8dvh solid rgba(255,255,255,0.2)",
                     borderRadius: "50%",
-                    borderLeftColor: "green",
-                    animation: `${spin} 1.2s ease-in-out infinite`,
-                }}>
-                </Sheet>
-                <Sheet sx={{
-                    height: "10dvh",
-                    width: "5dvh",
-                    borderTop: "1.8dvh solid green",
-                    borderRight: "1.8dvh solid green",
-                    left: "3dvh",
-                    top: "9dvh",
-                    transform: "scaleX(-1) rotate(135deg)",
-                    transformOrigin: "left top"
+                    clipPath: "rect(0dvh 20dvh 20dvh 10dvh)",
                 }}>
                     <Sheet sx={{
-
-                    }}>
-                    </Sheet>
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%",
+                        backgroundColor: "#008800",
+                        borderRadius: "50%",
+                        clipPath: "rect(0dvh 10dvh 20dvh 0dvh)",
+                        animation: `${spin} 400ms ease-in-out 1 forwards`,
+                    }}></Sheet>
                 </Sheet>
-            </Sheet>
+                <Sheet sx={{
+                    position: "absolute",
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "50%",
+                    clipPath: "rect(0dvh 20dvh 20dvh 10dvh)",
+                    animation: `${spin} 400ms ease-in-out 1 forwards`,
+                }}>
+                    <Sheet sx={{
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%",
+                        backgroundColor: "#008800",
+                        borderRadius: "50%",
+                        clipPath: "rect(0dvh 10dvh 20dvh 0dvh)",
+                        animation: `${spin} 400ms ease-in-out 1 forwards`,
+                    }}></Sheet>
+                </Sheet>
+                {/* <Sheet sx={{
+                    position: "absolute",
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "50%",
+                    backgroundColor: "pink",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}></Sheet> */}
+                <Sheet sx={{
+                    position: "absolute",
+                    height: "80%",
+                    width: "80%",
+                    borderRadius: "50%",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}></Sheet>
+                <Sheet sx={{
+                    display: "absolute",
+                    height: "5dvh",
+                    width: "10dvh",
+                    borderTop: "1.8dvh solid green",
+                    borderRight: "1.8dvh solid green",
+                    top: "45%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%) rotate(135deg)"
+                }}></Sheet>
+            </Sheet >
         </>
     )
 }
