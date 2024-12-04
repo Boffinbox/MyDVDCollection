2d1dc552751fcf5fe2789b95157c8a5a868bd124
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2024-12-04 15:02:48 +0000

    update scan text to hot-swap when deselecting collections

--- client/src/components/scanner/ScannerCollectionModal.tsx
+++ client/src/components/scanner/ScannerCollectionModal.tsx
@@ -78,6 +78,7 @@ export function ScannerCollectionModal(
                                             collectionId: ""
                                         };
                                     })
+                                    handleCollChange("")
                                     closeModal()
                                 }}
                             >
