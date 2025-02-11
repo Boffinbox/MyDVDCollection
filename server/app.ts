b182535d0348d32202370dc2b98ee2ddde85a671
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-02-11 11:26:35 +0000

    add express-mongo-sanitize to prevent keys having dollar signs

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const passport = require("passport")
+const mongoSanitize = require("express-mongo-sanitize")
 const helmet = require("helmet")
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(mongoSanitize());
 app.use(passport.initialize());
 
 // main router
