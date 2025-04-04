5a3316c1686ab554d5458b6fe154ff99fbd29eac
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-04 17:47:47 +0100

    add test to confirm barcodes route is working as intended

--- server/tests/referencedvds.test.ts
+++ server/tests/referencedvds.test.ts
@@ -6,6 +6,7 @@ const api = "/api/v1"
 
 // add defines here
 const userDVDFunctions = require("./helpers/userdvds")
+const discCollectionFunctions = require("./helpers/disccollections");
 
 test(`get a reference dvd`, async () =>
 {
@@ -41,4 +42,73 @@ test(`update a dvd's name to something different`, async () =>
     console.log(updateRefDvdRes.body)
     expect(updateRefDvdRes.body.barcode).toEqual("7321905737437")
     expect(updateRefDvdRes.body.title).toEqual("pp")
+})
+
+test(`get barcodes object`, async () =>
+{
+    const testSetup = await userDVDFunctions.testDVDSetup();
+    expect(testSetup.dvdRes.status).toBe(201);
+    expect(testSetup.dvdRes.body.referenceDVD.barcode).toEqual("7321905737437")
+    expect(testSetup.dvdRes.body.referenceDVD.title).toEqual("gremlins")
+
+    const title = "My Second Collection"
+    const res = await discCollectionFunctions.newCollection(testSetup.userToken, title);
+    expect(res.status).toBe(201);
+    expect(res.body.title).toEqual(title);
+
+    const collIdOne = testSetup.collId
+    const collIdTwo = res.body._id
+
+    console.log(collIdOne)
+    console.log(collIdTwo)
+
+    const disc1 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdOne,
+        "123456",
+        "test dvd one"
+    )
+    const disc2 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdOne,
+        "123456",
+        "test dvd pne"
+    )
+    const disc3 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdTwo,
+        "123456",
+        "test dvd one"
+    )
+    const disc4 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdOne,
+        "234567",
+        "test dvd two"
+    )
+    const disc5 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdOne,
+        "234567",
+        "test dvd two"
+    )
+    const disc6 = await userDVDFunctions.newDVD(
+        testSetup.userToken,
+        collIdOne,
+        "345678",
+        "test dvd three"
+    )
+
+    const barcodeRes = await request(app)
+        .get(`${api}/referencedvds/barcodes`)
+        .set(`Authorization`, `Bearer ${testSetup.userToken}`)
+    const data = barcodeRes.body
+    expect(data["123456"].count).toEqual(3)
+    expect(data["234567"].count).toEqual(2)
+    expect(data["345678"].count).toEqual(1)
+    expect(data["7321905737437"].count).toEqual(1)
+    expect(data["123456"].collArray.length).toEqual(2)
+    expect(data["7321905737437"].collArray.length).toEqual(1)
+    expect(data["123456"].collArray.indexOf(collIdOne)).toBeGreaterThan(-1)
+    expect(data["123456"].collArray.indexOf(collIdTwo)).toBeGreaterThan(-1)
 })
