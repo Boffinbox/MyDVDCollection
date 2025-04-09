ad1c29f39f1644f47e75f3d4327519df1c2d770c
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-09 18:46:23 +0100

    remove https requirement, render should re-add this in magically

--- server/server.ts
+++ server/server.ts
@@ -6,15 +6,8 @@ const fs = require("fs");
 const path = require("path");
 
 // Lastly, serve the app
-const port = 5000;
-const options = {
-    key: fs.readFileSync(path.join(__dirname, "certs/localhost-key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "certs/localhost.pem"))
-}
-
-const appHTTPS = https.createServer(options, app);
-
-appHTTPS.listen(port, () =>
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
 {
     console.log(`Server started on port ${port}`)
 })
