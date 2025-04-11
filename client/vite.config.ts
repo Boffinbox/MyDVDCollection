f7f65f39fd4c768cd3f8bb82247697d0612dc732
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-11 19:45:03 +0100

    update vite config to comment out testing localhost fake https

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,17 @@ export default defineConfig({
                 changeOrigin: true,
             }
         }
+        // proxy: {
+        //     "/api": {
+        //         target: "https://mydvdcollection-server.onrender.com",
+        //         changeOrigin: true,
+        //         secure: false
+        //     }
+        // },
+        // https: {
+        //     key: "certs/localhost-key.pem",
+        //     cert: "certs/localhost.pem"
+        // }
     },
     optimizeDeps: {
         exclude: ['@preflower/barcode-detector-polyfill']
