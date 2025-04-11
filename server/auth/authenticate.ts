b3dac2a50ba37f9294717aecfb91328e5e4d1658
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-11 20:56:15 +0100

    add domain to cookie options

--- server/auth/authenticate.ts
+++ server/auth/authenticate.ts
@@ -11,6 +11,7 @@ exports.COOKIE_OPTIONS = {
     signed: true,
     maxAge: eval(process.env.REFRESH_TOKEN_EXPIRY ? process.env.REFRESH_TOKEN_EXPIRY : "60 * 60 * 24 * 5") * 1000,
     sameSite: "none",
+    domain: ".mydvdcollection.onrender.com"
 }
 
 exports.getToken = function (user)
