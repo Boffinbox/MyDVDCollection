2f5e208f6cf2bb32405357bf380444e0892ec709
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-12 16:06:44 +0100

    update console log to inspect why the target request is undefined

--- client/src/httpverbs/GetAccessToken.tsx
+++ client/src/httpverbs/GetAccessToken.tsx
@@ -10,6 +10,7 @@ export async function GetAccessToken()
     try
     {
         console.log("Getting new access token...")
+        console.log(`${process.env.DOMAIN}/api/v1/users/refreshToken`)
         const response = await axios.post(`${process.env.DOMAIN}/api/v1/users/refreshToken`, config)
         console.log("Refresh request received.");
         console.log(response.data.token);
