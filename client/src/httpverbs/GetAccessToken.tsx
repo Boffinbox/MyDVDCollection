71fad5b647273982d9b32e45fb28b7450d0bf760
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-12 16:15:28 +0100

    update env variable to follow vite enforced convention

--- client/src/httpverbs/GetAccessToken.tsx
+++ client/src/httpverbs/GetAccessToken.tsx
@@ -10,8 +10,8 @@ export async function GetAccessToken()
     try
     {
         console.log("Getting new access token...")
-        console.log(`${process.env.DOMAIN}/api/v1/users/refreshToken`)
-        const response = await axios.post(`${process.env.DOMAIN}/api/v1/users/refreshToken`, config)
+        console.log(`${import.meta.env.VITE_API_URL}/api/v1/users/refreshToken`)
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/v1/users/refreshToken`, config)
         console.log("Refresh request received.");
         console.log(response.data.token);
         return response.data.token;
