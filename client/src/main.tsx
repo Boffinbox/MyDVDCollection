aab4de8f3b2b59528224c0282209a5b3a190dad6
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-14 17:50:43 +0100

    update re-add react strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client';
 import "@fontsource/inter"
 import "./index.css"
@@ -29,11 +29,11 @@ declare module '@tanstack/react-router' {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    // <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <ScrollContextProvider>
-            <RouterProvider router={router} context={{ queryClient }} />
-        </ScrollContextProvider>
-    </QueryClientProvider>
-    // </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ScrollContextProvider>
+                <RouterProvider router={router} context={{ queryClient }} />
+            </ScrollContextProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
 )
