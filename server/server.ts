89b755f28027740607195cc5923ca68379d81abf
Boffinbox <44021545+Boffinbox@users.noreply.github.com>
2025-04-14 16:15:17 +0100

    update to only read forged local test certificates when not in prod

--- server/server.ts
+++ server/server.ts
@@ -4,14 +4,9 @@ const app = require("./app.ts")
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
-const options = {
-    key: fs.readFileSync(path.join(__dirname, "certs/localhost-key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "certs/localhost.pem"))
-}
-const appHTTPS = https.createServer(options, app)
 const port = process.env.PORT || 5000;
 
-// Lastly, serve the app
+// serve the app
 if (process.env.NODE_ENV == "production")
 {
     app.listen(port, () =>
@@ -21,6 +16,11 @@ if (process.env.NODE_ENV == "production")
 }
 else
 {
+    const options = {
+        key: fs.readFileSync(path.join(__dirname, "certs/localhost-key.pem")),
+        cert: fs.readFileSync(path.join(__dirname, "certs/localhost.pem"))
+    }
+    const appHTTPS = https.createServer(options, app)
     appHTTPS.listen(port, () =>
     {
         console.log(`Local server started on port ${port}`)
